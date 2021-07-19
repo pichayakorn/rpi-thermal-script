5008c636d8854a13134806c661be946f13d37411
pichayakorn <82988679+pichayakorn@users.noreply.github.com>
2021-07-19 17:59:16 +0700

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,9 +1,9 @@
-printf "-15s%5s\n" "TIMESTAMP" "TEMP(degC)"
-printf "%20s\n" "----------------------"
+printf "%-15s%5s\n" "TIMESTAMP" "TEMP(degC)"
+printf "%20s\n" "-----------------------"
 while true
 do
-    temp-$(vcgencmd measure_temp | egrep -o '[0-9]*\.[e-9]*')
-    timestamp-$(date)
+    temp=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[e-9]*')
+    timestamp=$(date)
     printf "%-15s ,temp: %5s 'C\n" "$timestamp" "$temp"
     sleep 1
 done
