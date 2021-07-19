27abd73c78c2c1da0e14d4ca5c0420275490d130
pichayakorn <82988679+pichayakorn@users.noreply.github.com>
2021-07-19 17:55:28 +0700

    Create test_script.sh

---
+++ test_script.sh
@@ -0,0 +1,9 @@
+printf "-15s%5s\n" "TIMESTAMP" "TEMP(degC)"
+printf "%20s\n" "----------------------"
+while true
+do
+    temp-$(vcgencmd measure_temp | egrep -o '[0-9]*\.[e-9]*')
+    timestamp-$(date)
+    printf "%-15s ,temp: %5s 'C\n" "$timestamp" "$temp"
+    sleep 1
+done
